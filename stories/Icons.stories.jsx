a9a7627e5193c1fc1b09d79c657cf4da8272b291
Franck <franck.gaudin@gsoft.com>
2021-07-30 15:15:38 -0400

    chore: align components

--- stories/Icons.stories.jsx
+++ stories/Icons.stories.jsx
@@ -17,22 +17,22 @@ export const basic = () => (
 
 export const colored = () => (
   <>
-    <div>
+    <div style={{ display: 'flex', alignItems: 'flex-end' }}>
       <AddCalendar color="grey500" />
       <AddCalendar color="grey500" size="medium" />
       <AddCalendar color="grey500" size="large" />
     </div>
-    <div>
+    <div style={{ display: 'flex', alignItems: 'flex-end' }}>
       <AddCalendar color="grey600" />
       <AddCalendar color="grey600" size="medium" />
       <AddCalendar color="grey600" size="large" />
     </div>
-    <div>
+    <div style={{ display: 'flex', alignItems: 'flex-end' }}>
       <AddCalendar color="base" />
       <AddCalendar color="base" size="medium" />
       <AddCalendar color="base" size="large" />
     </div>
-    <div>
+    <div style={{ display: 'flex', alignItems: 'flex-end' }}>
       <AddCalendar color="interactive" />
       <AddCalendar color="interactive" size="medium" />
       <AddCalendar color="interactive" size="large" />
