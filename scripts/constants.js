ccdf19acc106507ea123e5012c45290802aa7938
Franck <franck.gaudin@gsoft.com>
2021-11-10 11:39:20 -0500

    chore: rework build process

--- scripts/constants.js
+++ scripts/constants.js
@@ -2,5 +2,12 @@ const ICONS_SOURCE_DIR = 'icon';
 const ROOT_DIR = 'dist';
 const COMPONENTS_DIR = 'components';
 const ICONS_DIST_DIR = `${ROOT_DIR}/svg`;
+const ICONS_SIZES = [16, 24, 32];
 
-module.exports = { ICONS_DIST_DIR, ICONS_SOURCE_DIR, ROOT_DIR, COMPONENTS_DIR };
+module.exports = {
+  ICONS_DIST_DIR,
+  ICONS_SOURCE_DIR,
+  ROOT_DIR,
+  COMPONENTS_DIR,
+  ICONS_SIZES,
+};
