b4b02515ba38d8414e439d8f7e0e53d4edf9f9a8
Franck <franck.gaudin@gsoft.com>
2022-06-15 16:50:19 +0200

    feature: add Grid and Rewind icon

---
+++ components/Rewind.jsx
@@ -0,0 +1,40 @@
+/* THIS FILE IS GENERATED. DO NOT EDIT IT. */
+import React from "react";
+import PropTypes from "prop-types";
+import parse from "html-react-parser";
+
+import { getSvgProps } from "./helper/get-svg-props";
+
+import './styles/icon.css';
+
+/**
+ * Renders a <Rewind /> component
+ *
+ * @param {Object} props
+ * @param {('small'|'medium'|'large')} [props.size=small] - the icon size
+ * @param {string} [props.ariaLabel] - the label for accessibility
+ */
+const Rewind = ({size, ariaLabel, className, ...other}) => {
+const svgData = {"16":"<svg width=\"16\" height=\"16\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M8 13a5 5 0 1 0-5-5\" stroke=\"#233043\" stroke-width=\"1.5\" stroke-linecap=\"round\" stroke-linejoin=\"round\"/><path d=\"M4.25 8 3.152 9.098a.333.333 0 0 1-.471 0L1.583 8\" stroke=\"#233043\" stroke-width=\"1.5\" stroke-linecap=\"round\" stroke-linejoin=\"round\"/></svg>","24":"<svg width=\"24\" height=\"24\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path fill-rule=\"evenodd\" clip-rule=\"evenodd\" d=\"M6.75 11.5A6.25 6.25 0 1 1 13 17.75a.75.75 0 0 0 0 1.5 7.75 7.75 0 1 0-7.75-7.75v.19l-.72-.72a.75.75 0 0 0-1.06 1.06l1.646 1.647a1.25 1.25 0 0 0 1.768 0L8.53 12.03a.75.75 0 1 0-1.06-1.06l-.72.72v-.19Z\" fill=\"#233043\"/></svg>","32":"<svg width=\"32\" height=\"32\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M17.333 25.333A9.333 9.333 0 1 0 8 16v1.333\" stroke=\"#233043\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"/><path d=\"m10.667 16-2.195 2.195a.667.667 0 0 1-.943 0L5.333 16\" stroke=\"#233043\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"/></svg>"}
+const props = getSvgProps({ size, ariaLabel, className, ...other });
+
+if (svgData[props.iconSize] === undefined) {
+  console.warn('this icon is not available in this size');
+  return <span className="empty"></span>;
+}
+
+const icon = parse(svgData[props.iconSize], props.options);
+return <>{icon}</>;
+};
+
+Rewind.propTypes = {
+  size: PropTypes.oneOf(["small", "medium", "large"]),
+  ariaLabel: PropTypes.string,
+};
+
+Rewind.defaultProps = {
+  size: "medium",
+  ariaLabel: "",
+};
+
+export default Rewind;
