88b8a6806dbacf4c96a47b03c9f1ca236ff7fd3d
Franck Gaudin <franck.gaudin@gsoft.com>
2022-06-23 20:28:42 +0200

    Fix/remove solid (#39)
    * fix: rename Remove-Solid to Close-Solid
    
    * fix: add new Remove-Solid icons
    
    * chore: add changeset

---
+++ .changeset/wet-hats-deny.md
@@ -0,0 +1,5 @@
+---
+'@igloo-ui/icons': major
+---
+
+BRAKING CHANGE the `RemoveSolid` icon change and adding a new `CloseSolid`
---
+++ components/CloseSolid.jsx
@@ -0,0 +1,40 @@
+/* THIS FILE IS GENERATED. DO NOT EDIT IT. */
+import React from "react";
+import PropTypes from "prop-types";
+import parse from "html-react-parser";
+
+import { getSvgProps } from "./helper/get-svg-props";
+
+import './styles/icon.css';
+
+/**
+ * Renders a <CloseSolid /> component
+ *
+ * @param {Object} props
+ * @param {('small'|'medium'|'large')} [props.size=small] - the icon size
+ * @param {string} [props.ariaLabel] - the label for accessibility
+ */
+const CloseSolid = ({size, ariaLabel, className, ...other}) => {
+const svgData = {"16":"<svg width=\"16\" height=\"16\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path fill-rule=\"evenodd\" clip-rule=\"evenodd\" d=\"M14 8A6 6 0 1 1 2 8a6 6 0 0 1 12 0Zm-3.22-2.78a.75.75 0 0 1 0 1.06L9.06 8l1.72 1.72a.75.75 0 1 1-1.06 1.06L8 9.06l-1.72 1.72a.75.75 0 1 1-1.06-1.06L6.94 8 5.22 6.28a.75.75 0 0 1 1.06-1.06L8 6.94l1.72-1.72a.75.75 0 0 1 1.06 0Z\" fill=\"#233043\"/></svg>","24":"<svg width=\"24\" height=\"24\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path fill-rule=\"evenodd\" clip-rule=\"evenodd\" d=\"M12 20a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm3.53-11.53a.75.75 0 0 1 0 1.06L13.06 12l2.47 2.47a.75.75 0 1 1-1.06 1.06L12 13.06l-2.47 2.47a.75.75 0 0 1-1.06-1.06L10.94 12 8.47 9.53a.75.75 0 0 1 1.06-1.06L12 10.94l2.47-2.47a.75.75 0 0 1 1.06 0Z\" fill=\"#233043\"/></svg>","32":"<svg width=\"32\" height=\"32\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path fill-rule=\"evenodd\" clip-rule=\"evenodd\" d=\"M26.667 16c0 5.891-4.776 10.667-10.667 10.667S5.333 21.89 5.333 16 10.11 5.333 16 5.333 26.667 10.11 26.667 16Zm-5.96-4.707a1 1 0 0 1 0 1.414L17.414 16l3.293 3.293a1 1 0 0 1-1.414 1.414L16 17.414l-3.293 3.293a1 1 0 0 1-1.414-1.414L14.586 16l-3.293-3.293a1 1 0 0 1 1.414-1.414L16 14.586l3.293-3.293a1 1 0 0 1 1.414 0Z\" fill=\"#233043\"/></svg>"}
+const props = getSvgProps({ size, ariaLabel, className, ...other });
+
+if (svgData[props.iconSize] === undefined) {
+  console.warn('this icon is not available in this size');
+  return <span className="empty"></span>;
+}
+
+const icon = parse(svgData[props.iconSize], props.options);
+return <>{icon}</>;
+};
+
+CloseSolid.propTypes = {
+  size: PropTypes.oneOf(["small", "medium", "large"]),
+  ariaLabel: PropTypes.string,
+};
+
+CloseSolid.defaultProps = {
+  size: "medium",
+  ariaLabel: "",
+};
+
+export default CloseSolid;
